988dafde21c03b3195e02e6c2f14355341f384e8
ainizainudinn <38076691+ainizainudinn@users.noreply.github.com>
2018-12-13 01:34:09 +0800

    Improvements of Interface
    I put some additional method to make the method looping.

--- CurrencyConverter/src/currencyconverter/CurrencyConverter.java
+++ CurrencyConverter/src/currencyconverter/CurrencyConverter.java
@@ -30,6 +30,26 @@ public class CurrencyConverter implements CurrencyConverterIfce, ComponentInterf
 			rate = 7.85;
 			break;
 			
+		case "MYRU":
+			rate = 1/0.24;
+			break;
+			
+		case "MYRC":
+			rate = 1/0.32;
+			break;
+			
+		case "MYRJ":
+			rate = 1/27.19;
+			break;
+			
+		case "MYRS":
+			rate = 1/0.33;
+			break;
+			
+		case "MYRT":
+			rate = 1/7.85;
+			break;
+			
 		default:
 			rate = 1.00;
 			break;
@@ -37,4 +57,6 @@ public class CurrencyConverter implements CurrencyConverterIfce, ComponentInterf
 		
 		return amount * rate;
 	}
+	
+	
 }
--- CurrencyConverterApp/src/currencyconverter/app/CurrencyConverterApp.java
+++ CurrencyConverterApp/src/currencyconverter/app/CurrencyConverterApp.java
@@ -12,51 +12,149 @@ public class CurrencyConverterApp {
 	
 	public static void main(String [] args) {
 		currencyConverter = (CurrencyConverterIfce) ComponentRegistry.fetchComponent(CurrencyConverterIfce.class);
-		
+		System.out.println("Welcome to Currency Converter!");
+		getAmount();
+	
+	}
+	
+	public static void getAmount() {
 		scanner = new Scanner(System.in);
-		
-		System.out.println("What is the amount of money you wish to convert? ");
-		double amount = scanner.nextInt();
-		
-		System.out.println("What currency you wish to convert to?"
-				+ "\n 1. USA"
-				+ "\n 2. CND"
-				+ "\n 3. JPN"
-				+ "\n 4. SGD"
-				+ "\n 5. THB");
+		System.out.println("Enter currency you wish to convert:"
+				+ "\n 1. MYR to USA"
+				+ "\n 2. MYR to CND"
+				+ "\n 3. MYR to JPN"
+				+ "\n 4. MYR to SGD"
+				+ "\n 5. MYR to THB"
+				+ "\n 6. USA to MYR"
+				+ "\n 7. CND to MYR"
+				+ "\n 8. JPN to MYR"
+				+ "\n 9. SGD to MYR"
+				+ "\n 10. THB to MYR");
 		int choice = scanner.nextInt();
-		
+		double amount, newCurrency;	
 		String currency = "";
 		
 		switch(choice) {
 		case 1:
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
 			currency = "USD";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("RM" + amount + " get you " + currency + newCurrency);
+			choose();
 			break;
 			
 		case 2:
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
 			currency = "CND";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("RM" + amount + " get you " + currency + newCurrency);
+			choose();
 			break;
 			
+			
 		case 3:
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
 			currency = "JPN";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("RM" + amount + " get you " + currency + newCurrency);
+			choose();
 			break;
 			
 		case 4:
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
 			currency = "SGD";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("RM" + amount + " get you " + currency + newCurrency);
+			choose();
 			break;
 			
 		case 5: 
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
 			currency = "THB";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("RM" + amount + " get you " + currency + newCurrency);
+			choose();
+			break;
+			
+		case 6: 
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
+			currency = "MYRU";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("USA" + amount + " get you RM"+ newCurrency);
+			choose();
+			break;
+			
+		case 7: 
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
+			currency = "MYRC";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("CND" + amount + " get you RM" + newCurrency);
+			choose();
+			break;
+			
+		case 8: 
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
+			currency = "MYRJ";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("JPN" + amount + " get you RM" + newCurrency);
+			choose();
+			break;
+			
+		case 9: 
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
+			currency = "MYRS";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("SGD" + amount + " get you RM"+ newCurrency);
+			choose();
+			break;
+			
+		case 10: 
+			System.out.println("Enter amount you wish to convert:");
+			amount = scanner.nextInt();
+			currency = "MYRT";
+			newCurrency = currencyConverter.convertCurrency(amount, currency);
+			System.out.println("THB" + amount + " get you RM" + newCurrency);
+			choose();
 			break;
 			
 			default: 
-				System.out.println("Invalid currency");
+				System.out.println("You enter the wrong input. Invalid currency. Please try again");
+				getAmount();
 				break;
 		}
 		
-		double newCurrency = currencyConverter.convertCurrency(amount, currency);
 		
-		System.out.println("RM" + amount + " get you " + currency + newCurrency);
+	}
+	
+	public static void choose() {
+		scanner = new Scanner(System.in);
+		System.out.println("Enter your option:"
+				+ "\n 1. Convert other currency"
+				+ "\n 2. Exit");
+		int choose = scanner.nextInt();
 		
+		switch(choose) {
+		case 1: 
+			getAmount();
+			break;
+			
+		case 2:
+			System.out.println("Exit");
+			break;
+			
+		default:
+			System.out.println("You enter the wrong input. Try again!");
+			choose();
+			break;
+		}
 	}
 }
